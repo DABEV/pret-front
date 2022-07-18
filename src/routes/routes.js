5e4b552cf9d389dd8e64fa3a6988cfcab61830b4
vimillan <20193tn066@utez.edu.mx>
2022-07-18 13:43:46 -0500

    feat: Modify route file
    Add path for status view (Postulaciones)

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,7 @@
 import ProfileLayout from "@/layout/ProfileLayout"
 import Profile from "@/components/Profile.vue"
+import Status from "@/components/Status.vue"
+import StatusLayout from "@/layout/StatusLayout.vue"
 
 const routes = [
     
@@ -25,6 +27,19 @@ const routes = [
                 path: "",
                 name: "Profile",
                 component: Profile,
+            }
+        ]
+
+    },
+    {
+        path: "/postulaciones",
+        component:StatusLayout,
+        redirect: "/postulaciones",
+        children: [
+            {
+                path: "",
+                name: "Postulaciones",
+                component: Status,
             }
         ]
 
