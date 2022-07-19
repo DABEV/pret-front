2d0811bb4f944509d20dccf86dccfb6c5aca94da
Dafne <20193tn029@utez.edu.mx>
2022-07-19 01:45:01 -0500

    added error view and its styles

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,26 @@
 
 const routes = [
-    
+
     {
         path: "/",
-        component:() => import("@/components/HelloWorld.vue"),
+        component: () => import("@/components/HelloWorld.vue"),
         redirect: "/",
         children: [
             {
                 path: "",
                 name: "Example",
-                component:() => import("@/components/HelloWorld.vue") ,
+                component: () => import("@/components/HelloWorld.vue"),
+            }
+        ],
+        component: () => import("@/components/error-view/index.vue"),
+        redirect: "/error",
+        children: [
+            {
+                path: "",
+                name: "Example",
+                component: () => import("@/components/error-view/index.vue"),
             }
         ]
-
     },
 ]
 
