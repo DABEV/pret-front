7f8666ac79f39f558f81262e47ac8e9da976d9fd
vimillan <20193tn066@utez.edu.mx>
2022-07-20 14:17:27 -0500

    feat: Add CacanciestList view
    Lista created include add and modify button

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ import ContentLayout from "@/layout/ContentLayout"
 import Profile from "@/components/Profile.vue"
 import Status from "@/components/Status.vue"
 import ContactList from "@/components/ContactList.vue"
+import VacanciesList from "@/components/VacanciesList.vue"
 
 const routes = [
     
@@ -54,6 +55,20 @@ const routes = [
                 path: "",
                 name: "Contactos",
                 component: ContactList,
+            }
+        ]
+
+    },
+
+    {
+        path: "/vacantes",
+        component:ContentLayout,
+        redirect: "/vacantes",
+        children: [
+            {
+                path: "",
+                name: "Vacantes",
+                component: VacanciesList,
             }
         ]
 
