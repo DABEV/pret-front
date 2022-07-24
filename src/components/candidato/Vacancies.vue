ce65346a4d047d53719efd37e5ca29f02f8fccd9
vimillan <20193tn066@utez.edu.mx>
2022-07-24 11:39:02 -0500

    style: Modify style classes and view details
    Add order opcions and dialogs

--- src/components/candidato/Vacancies.vue
+++ src/components/candidato/Vacancies.vue
@@ -54,9 +54,11 @@
                   <vs-th
                     class="custom-th"
                     sort
-                    @click="vacantes = $vs.sortData($event, vacantes, 'inicio')"
+                    @click="
+                      vacantes = $vs.sortData($event, vacantes, 'sueldoMin')
+                    "
                   >
-                    Publicación
+                    Sueldo mínimo
                   </vs-th>
                   <div class="divider">
                     <span class="border"></span>
@@ -74,6 +76,32 @@
                     <span class="border"></span>
                   </div>
                 </vs-col>
+                <vs-col class="item">
+                  <vs-th
+                    class="custom-th"
+                    sort
+                    @click="
+                      vacantes = $vs.sortData($event, vacantes, 'modoPago')
+                    "
+                  >
+                    Modo de pago
+                  </vs-th>
+                  <div class="divider">
+                    <span class="border"></span>
+                  </div>
+                </vs-col>
+                <vs-col class="item">
+                  <vs-th
+                    class="custom-th"
+                    sort
+                    @click="vacantes = $vs.sortData($event, vacantes, 'estado')"
+                  >
+                    Estado
+                  </vs-th>
+                  <div class="divider">
+                    <span class="border"></span>
+                  </div>
+                </vs-col>
               </vs-row>
             </vs-tr>
           </div>
@@ -86,7 +114,6 @@
             >
               No se encontraron registros
             </div>
-
             <div
               :key="i"
               :data="v"
@@ -97,47 +124,55 @@
               )"
               class="item"
             >
-              <vs-row class="space">
-                <vs-col lg="2" sm="12" xs="12" class="text-center space-top">
-                  <vs-avatar color="#b13cd2" class="margin-xy">
+              <vs-row justify="space-between" class="space">
+                <vs-col lg="1" sm="2" xs="2" class="space-top">
+                  <vs-avatar color="#b13cd2" class="space-top end-item">
                     <template #text> {{ v.empresa }} </template>
                   </vs-avatar>
                 </vs-col>
                 <vs-col lg="9" sm="12" xs="12" class="space-top">
-                  <p>
-                    <b>{{ v.nombre }}</b>
-                  </p>
-                  <vs-row>
-                    <vs-col w="3">
-                      <small class="bg-primary">{{ v.empresa }}</small>
-                    </vs-col>
-                    <vs-col w="3">
-                      <small class="bg-gray">{{ v.estado }}</small>
-                    </vs-col>
-                    <vs-col w="6">
-                      <small class="bg-sec"> Vigencia: {{ v.fin }}</small>
-                    </vs-col>
-                  </vs-row>
-                  <br />
-                  <small class="bg-gray">{{ v.descripcion }}</small>
-                  <br />
-                  <vs-row justify="space-between" class="space-top">
-                    <vs-col lg="6" sm="3" xs="3">
-                      <p>
-                        Mínimo:
-                        <small class="badge-pri"> ${{ v.sueldoMin }}</small>
-                      </p>
-                    </vs-col>
-                    <vs-col lg="6" sm="3" xs="3">
-                      <p>
-                        Máximo:
-                        <small class="badge-war"> ${{ v.sueldoMax }}</small>
-                      </p>
-                    </vs-col>
-                  </vs-row>
+                  <div
+                    class="item-click"
+                    @click="Detalles(v), (active = !active)"
+                  >
+                    <p class="space-top">
+                      <b>{{ v.nombre }}</b>
+                    </p>
+                    <vs-row class="space">
+                      <vs-col w="3">
+                        <small class="bg-primary">{{ v.empresa }}</small>
+                      </vs-col>
+                      <vs-col w="3">
+                        <small class="bg-gray"> {{ v.estado }}</small>
+                      </vs-col>
+                      <vs-col w="6">
+                        <small class="bg-sec"> Vigencia: {{ v.fin }}</small>
+                      </vs-col>
+                    </vs-row>
+                    <small class="bg-gray">{{ v.descripcion }}</small>
+                    <vs-row justify="space-between" class="space-top space">
+                      <vs-col lg="4" sm="3" xs="3">
+                        <p class="bold">
+                          {{ v.modoPago }}
+                        </p>
+                      </vs-col>
+                      <vs-col lg="4" sm="3" xs="3">
+                        <p>
+                          Mínimo:
+                          <small class="badge-pri"> ${{ v.sueldoMin }}</small>
+                        </p>
+                      </vs-col>
+                      <vs-col lg="4" sm="3" xs="3">
+                        <p>
+                          Máximo:
+                          <small class="badge-war"> ${{ v.sueldoMax }}</small>
+                        </p>
+                      </vs-col>
+                    </vs-row>
+                  </div>
                 </vs-col>
                 <vs-col lg="1" sm="12" xs="12" class="space-top">
-                  <vs-row justify="space-between">
+                  <vs-row justify="space-between" class="space-top">
                     <vs-col lg="12" sm="2" xs="2">
                       <vs-tooltip>
                         <vs-button
@@ -155,7 +190,12 @@
                     </vs-col>
                     <vs-col lg="12" sm="2" xs="2">
                       <vs-tooltip>
-                        <vs-button icon animation-type="vertical" danger>
+                        <vs-button
+                          icon
+                          animation-type="vertical"
+                          danger
+                          @click="favoritos = !favoritos"
+                        >
                           <i class="bx bx-heart"></i>
                           <template #animate>
                             <i class="bx bxs-heart"></i>
@@ -181,6 +221,18 @@
         </vs-col>
       </vs-row>
     </div>
+
+    <vs-dialog prevent-close scroll width="550px" v-model="active">
+      <template #header>
+        <h4>{{ vacante.nombre }}</h4>
+      </template>
+    </vs-dialog>
+
+    <vs-dialog prevent-close v-model="favoritos">
+      <template #header>
+        <h4>Añadir a favoritos</h4>
+      </template>
+    </vs-dialog>
   </div>
 </template>
 
@@ -189,9 +241,11 @@ export default {
   name: "VacanciesList",
   data: () => ({
     active: false,
+    favoritos: false,
     page: 1,
     max: 5,
     search: "",
+    vacante: {},
     sortIcon1: true,
     sortIcon2: true,
     sortIcon3: true,
@@ -203,9 +257,9 @@ export default {
         tipo: "Medio tiempo",
         empresa: "Microsoft",
         modalidad: "Híbrido",
-        modoPago: "Quincenal",
-        sueldoMin: 20000,
-        sueldoMax: 50000,
+        modoPago: "Mensual",
+        sueldoMin: 40000,
+        sueldoMax: 80000,
         inicio: "5/19/12",
         fin: "7/19/12",
         descripcion:
@@ -248,7 +302,7 @@ export default {
         tipo: "Medio tiempo",
         empresa: "Sony",
         modalidad: "Híbrido",
-        modoPago: "Quincenal",
+        modoPago: "Semanal",
         sueldoMin: 20000,
         sueldoMax: 50000,
         inicio: "5/19/12",
@@ -272,15 +326,15 @@ export default {
           "Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.",
       },
       {
-        nombre: "Desarrollador Full-stack PHP",
+        nombre: "Analista de datos",
         reclutador: "Roberto Miramontes Ruiseñor",
-        estado: "Morelos",
-        tipo: "Medio tiempo",
+        estado: "Nayarit",
+        tipo: "Tiempo completo",
         empresa: "Rappi",
         modalidad: "Híbrido",
-        modoPago: "Quincenal",
-        sueldoMin: 20000,
-        sueldoMax: 50000,
+        modoPago: "Mensual",
+        sueldoMin: 200050,
+        sueldoMax: 500000,
         inicio: "5/19/12",
         fin: "7/19/12",
         descripcion:
@@ -304,8 +358,8 @@ export default {
     ],
   }),
   methods: {
-    Confirmar: function (contacto) {
-      this.contacto = contacto;
+    Detalles: function (vacante) {
+      this.vacante = vacante;
     },
   },
 };
