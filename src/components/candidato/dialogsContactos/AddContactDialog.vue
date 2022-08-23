29dfd1cf7b2f0a1e58d2a591a7aba9a7fadb667b
vimillan <20193tn066@utez.edu.mx>
2022-08-23 16:55:05 -0500

    refactor: contact structure modified

---
+++ src/components/candidato/dialogsContactos/AddContactDialog.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <vs-tooltip>
+      <vs-button @click="Añadir()" icon animation-type="rotate" color="#B13CD2">
+        <i class="bx bx-user-plus"></i>
+        <template #animate>
+          <i class="bx bxs-user-plus"></i>
+        </template>
+      </vs-button>
+      <template #tooltip> Añadir contacto </template>
+    </vs-tooltip>
+    <vs-dialog v-model="activeAdd">
+      <template #header>
+        <h2>Añadir Contacto</h2>
+      </template>
+      <div class="margin-xy space text-center">
+        <small class="bg-sec">Tu solicitúd deberá ser aceptada</small>
+        <vs-input
+          class="space-top space"
+          placeholder="Correo del usuario"
+          v-model="correoContacto"
+          color="#1e88e5"
+          block
+        >
+          <template #icon>
+            <i class="bx bxs-contact"></i>
+          </template>
+        </vs-input>
+      </div>
+      <template #footer>
+        <vs-row justify="center">
+          <vs-col lg="5" sm="12" xs="12" class="space-top center-item">
+            <vs-button transparent dark @click="activeAdd = false" block>
+              Cancelar
+            </vs-button>
+          </vs-col>
+          <vs-col lg="5" sm="12" xs="12" class="space-top center-item">
+            <vs-button primary block @click="enviarAdd()">
+              Enviar datos
+            </vs-button>
+          </vs-col>
+        </vs-row>
+      </template>
+    </vs-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactDetailDialog",
+  data: () => ({
+    activeAdd: false,
+    correoContacto: "",
+  }),
+  methods: {
+    Añadir: function () {
+      this.correoContacto = "";
+      this.activeAdd = true;
+    },
+    enviarAdd: function () {},
+  },
+};
+</script>
---
+++ src/components/candidato/dialogsContactos/MyContactList.vue
@@ -0,0 +1,187 @@
+<template>
+  <div class="space-top content-data space datos text-start bg-gray">
+    <vs-row justify="space-between" class="space">
+      <vs-col lg="2">
+        <vs-button success block>
+          Enviadas:
+          <vs-avatar circle size="25" class="margin-xy">
+            {{ solicitudes.length }}
+          </vs-avatar>
+        </vs-button>
+      </vs-col>
+      <vs-col lg="2">
+        <vs-button primary block>
+          Por aceptar:
+          <vs-avatar circle size="25" class="margin-xy">
+            {{ enviadas.length }}
+          </vs-avatar>
+        </vs-button>
+      </vs-col>
+    </vs-row>
+    <h3 class="text-center">Tus contactos</h3>
+    <div class="center" v-if="$vs.getSearch(contactos, search).length < 1">
+      No se encontraron registros
+    </div>
+    <div>
+      <div
+        :key="i"
+        :data="c"
+        v-for="(c, i) in $vs.getPage(
+          $vs.getSearch(contactos, search),
+          page,
+          max
+        )"
+        class="item"
+      >
+        <vs-row class="space" justify="space-between">
+          <vs-col lg="1" sm="2" xs="2" class="space-top">
+            <vs-avatar class="end-item" size="50">
+              <img :src="c.foto" alt="" />
+            </vs-avatar>
+          </vs-col>
+          <vs-col lg="9" sm="9" xs="9" class="space-top">
+            <div
+              class="item-click"
+              @click="Confirmar(c), (detalles = !detalles)"
+            >
+              <p>
+                <b
+                  >{{ c.nombre }} {{ c.apellidoMaterno }}
+                  {{ c.apellidoPaterno }}</b
+                >
+              </p>
+              <small>{{ c.correoElectronico }}</small>
+            </div>
+          </vs-col>
+          <vs-col lg="1" sm="12" xs="12" class="space-top">
+            <vs-tooltip>
+              <vs-button
+                icon
+                animation-type="rotate"
+                danger
+                @click="(active = !active), Confirmar(c)"
+              >
+                <i class="bx bx-trash-alt"></i>
+                <template #animate>
+                  <i class="bx bxs-trash-alt"></i>
+                </template>
+              </vs-button>
+              <template #tooltip> Quitar </template>
+            </vs-tooltip>
+          </vs-col>
+        </vs-row>
+        <div class="divider">
+          <span class="border"></span>
+        </div>
+      </div>
+    </div>
+    <div class="pagination">
+      <vs-pagination
+        v-model="page"
+        :length="$vs.getLength($vs.getSearch(contactos, search), max)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactList",
+  data: () => ({
+    active: false,
+    detalles: false,
+    page: 1,
+    max: 5,
+    contactos: [
+      {
+        nombre: "Cameron",
+        apellidoMaterno: "Williamson",
+        apellidoPaterno: "Warren",
+        correoElectronico: "cameron@example.com",
+        telefono: "(704) 555-0127",
+        estadoRepublica: {
+          nombre: "Morelos",
+        },
+        tituloCurricular: "Administradora de base de datos (DBA)",
+        fechaNacimiento: "9/4/12",
+        foto: "https://images.unsplash.com/photo-1484186139897-d5fc6b908812?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+      },
+      {
+        nombre: "Esther",
+        apellidoMaterno: "Howard",
+        apellidoPaterno: "Simmons",
+        correoElectronico: "esther@example.com",
+        telefono: "(907) 555-0101",
+        estadoRepublica: {
+          nombre: "Morelos",
+        },
+        tituloCurricular: "Administradora de base de datos (DBA)",
+        fechaNacimiento: "9/4/12",
+        foto: "https://images.unsplash.com/photo-1518577915332-c2a19f149a75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=562&q=80",
+      },
+      {
+        nombre: "Robert",
+        apellidoMaterno: "Warren",
+        apellidoPaterno: "Hawkins",
+        correoElectronico: "robert@example.com",
+        telefono: "(217) 555-0113",
+        estadoRepublica: {
+          nombre: "Querétaro",
+        },
+        tituloCurricular: "Administradora de base de datos (DBA)",
+        fechaNacimiento: "9/4/12",
+        foto: "https://images.unsplash.com/photo-1529068755536-a5ade0dcb4e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=581&q=80",
+      },
+      {
+        nombre: "Alexander",
+        apellidoMaterno: "Edwards",
+        apellidoPaterno: "Wade",
+        correoElectronico: "alexander@example.com",
+        telefono: "(308) 555-0121",
+        estadoRepublica: {
+          nombre: "Durango",
+        },
+        tituloCurricular: "Administradora de base de datos (DBA)",
+        fechaNacimiento: "9/4/12",
+        foto: "https://images.unsplash.com/photo-1485528562718-2ae1c8419ae2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=558&q=80",
+      },
+      {
+        nombre: "Leslie",
+        apellidoMaterno: "Brooklyn",
+        apellidoPaterno: "Williamson",
+        correoElectronico: "leslie@example.com",
+        telefono: "(302) 555-0107",
+        estadoRepublica: {
+          nombre: "Tamaulipas",
+        },
+        tituloCurricular: "Administradora de base de datos (DBA)",
+        fechaNacimiento: "9/4/12",
+        foto: "https://images.unsplash.com/photo-1485893086445-ed75865251e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+      },
+      {
+        nombre: "Ralph",
+        apellidoMaterno: "Williamson",
+        apellidoPaterno: "Fox",
+        correoElectronico: "ralph@example.com",
+        telefono: "(702) 555-0122",
+        estadoRepublica: {
+          nombre: "Oaxaca",
+        },
+        tituloCurricular: "Administradora de base de datos (DBA)",
+        fechaNacimiento: "9/4/12",
+        foto: "https://images.unsplash.com/photo-1483995564125-85915c11dcfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=641&q=80",
+      },
+    ],
+  }),
+  methods: {
+    Confirmar: function (contacto) {
+      this.contacto = contacto;
+      this.estado = contacto.estadoRepublica;
+    },
+    CloseDetails: function () {
+      this.detalles = !this.detalles;
+    },
+  },
+  props: {},
+};
+</script>
