70ed65047c07e0fabbc5f60c442bec377812b3ff
vimillan <20193tn066@utez.edu.mx>
2022-08-25 15:17:35 -0500

    fix: static contac list deleted

--- src/components/candidato/dialogsVacante/ApplyDialog.vue
+++ src/components/candidato/dialogsVacante/ApplyDialog.vue
@@ -62,92 +62,7 @@ export default {
       cv: "",
     },
     candidato: {},
-    contactos: [
-      {
-        nombre: "Cameron",
-        apellidoMaterno: "Williamson",
-        apellidoPaterno: "Warren",
-        correoElectronico: "cameron@example.com",
-        telefono: "(704) 555-0127",
-        estadoRepublica: {
-          nombre: "Morelos",
-        },
-        tituloCurricular:
-          "Grado de estudios: Administradora de base de datos (DBA)",
-        fechaNacimiento: "9/4/12",
-        foto: "https://images.unsplash.com/photo-1484186139897-d5fc6b908812?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
-      },
-      {
-        nombre: "Esther",
-        apellidoMaterno: "Howard",
-        apellidoPaterno: "Simmons",
-        correoElectronico: "esther@example.com",
-        telefono: "(907) 555-0101",
-        estadoRepublica: {
-          nombre: "Morelos",
-        },
-        tituloCurricular:
-          "Grado de estudios: Administradora de base de datos (DBA)",
-        fechaNacimiento: "9/4/12",
-        foto: "https://images.unsplash.com/photo-1518577915332-c2a19f149a75?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=562&q=80",
-      },
-      {
-        nombre: "Robert",
-        apellidoMaterno: "Warren",
-        apellidoPaterno: "Hawkins",
-        correoElectronico: "robert@example.com",
-        telefono: "(217) 555-0113",
-        estadoRepublica: {
-          nombre: "Querétaro",
-        },
-        tituloCurricular:
-          "Grado de estudios: Administradora de base de datos (DBA)",
-        fechaNacimiento: "9/4/12",
-        foto: "https://images.unsplash.com/photo-1529068755536-a5ade0dcb4e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=581&q=80",
-      },
-      {
-        nombre: "Alexander",
-        apellidoMaterno: "Edwards",
-        apellidoPaterno: "Wade",
-        correoElectronico: "alexander@example.com",
-        telefono: "(308) 555-0121",
-        estadoRepublica: {
-          nombre: "Durango",
-        },
-        tituloCurricular:
-          "Grado de estudios: Administradora de base de datos (DBA)",
-        fechaNacimiento: "9/4/12",
-        foto: "https://images.unsplash.com/photo-1485528562718-2ae1c8419ae2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=558&q=80",
-      },
-      {
-        nombre: "Leslie",
-        apellidoMaterno: "Brooklyn",
-        apellidoPaterno: "Williamson",
-        correoElectronico: "leslie@example.com",
-        telefono: "(302) 555-0107",
-        estadoRepublica: {
-          nombre: "Tamaulipas",
-        },
-        tituloCurricular:
-          "Grado de estudios: Administradora de base de datos (DBA)",
-        fechaNacimiento: "9/4/12",
-        foto: "https://images.unsplash.com/photo-1485893086445-ed75865251e0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
-      },
-      {
-        nombre: "Ralph",
-        apellidoMaterno: "Williamson",
-        apellidoPaterno: "Fox",
-        correoElectronico: "ralph@example.com",
-        telefono: "(702) 555-0122",
-        estadoRepublica: {
-          nombre: "Oaxaca",
-        },
-        tituloCurricular:
-          "Grado de estudios: Administradora de base de datos (DBA)",
-        fechaNacimiento: "9/4/12",
-        foto: "https://images.unsplash.com/photo-1483995564125-85915c11dcfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=641&q=80",
-      },
-    ],
+    contactos: [],
     pdf: null,
   }),
   methods: {
@@ -247,8 +162,8 @@ export default {
   props: {
     vacante: Object,
   },
-  mounted(){
+  mounted() {
     this.CargarSession();
-  }
+  },
 };
 </script>
